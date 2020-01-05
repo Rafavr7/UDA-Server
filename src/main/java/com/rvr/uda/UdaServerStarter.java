c73c80b92518e7c065a5f0d316a067916d920c24
Rafael Vieira Rangel <rafaelvrangel@gmail.com>
2020-01-05 19:25:45 +0000

    Project's initial commit

---
+++ src/main/java/com/rvr/uda/UdaServerStarter.java
@@ -0,0 +1,23 @@
+package com.rvr.uda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class UdaServerStarter {
+    
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+	SpringApplication.run(UdaServerStarter.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/UDA/UdaServerStarterTests.java
@@ -0,0 +1,13 @@
+package com.example.UDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdaServerStarterTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
