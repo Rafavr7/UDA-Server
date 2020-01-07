8d2fb47c089ea9d8a45e76979e5b0c1c6bb6a846
Rafael Vieira Rangel <rafaelvrangel@gmail.com>
2020-01-07 21:24:43 +0000

    Started app splash screen

--- src/main/java/com/rvr/uda/UdaServerStarter.java
+++ src/main/java/com/rvr/uda/UdaServerStarter.java
@@ -3,6 +3,7 @@ package com.rvr.uda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,6 +19,11 @@ public class UdaServerStarter {
 
     public static void main(String[] args) {
 	SpringApplication.run(UdaServerStarter.class, args);
+    }
+    
+    @GetMapping("/home")
+    public String homePage() {
+        return "home";
     }
 
 }
